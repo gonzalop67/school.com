1d1058cc37ce997d5a245649dac1682459744a87
Gonzalo Nicolás Peñaherrera Escobar <gonzalop67@gmail.com>
2025-08-15 09:28:21 -0500

    HTML Template Setup (Layouts Include Sidebar, Header, Footer)

---
+++ resources/views/backend/dashboard.blade.php
@@ -0,0 +1,26 @@
+@extends('backend.layouts.app')
+
+@section('content')
+    <!-- START BREADCRUMB -->
+    <ul class="breadcrumb">
+        <li><a href="#">Home</a></li>
+        <li class="active">Dashboard</li>
+    </ul>
+    <!-- END BREADCRUMB -->
+
+    <!-- PAGE TITLE -->
+    <div class="page-title">
+        <h2><span class="fa fa-arrow-circle-o-left"></span> Dashboard</h2>
+    </div>
+    <!-- END PAGE TITLE -->
+
+    <!-- PAGE CONTENT WRAPPER -->
+    <div class="page-content-wrap">
+
+    </div>
+    <!-- END PAGE CONTENT WRAPPER -->
+@endsection
+
+@section('script')
+    <script type="text/javascript" src="{{ url('public/js/demo_tables.js') }}"></script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Backend\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthController::class, 'login']);
 Route::get('forgot', [AuthController::class, 'forgot']);
+Route::get('panel/dashboard', [DashboardController::class, 'dashboard']);
+
